b00c990ab6fbf376421c58af55629dfa213c0dda
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-11 21:39:41 +0100

    Add attr_accessor :nameable in Decorator class

--- decorator.rb
+++ decorator.rb
@@ -1,12 +1,10 @@
 require_relative 'nameable'
 
 class Decorator < Nameable
+  attr_accessor :nameable
+
   def initialize(nameable)
     super()
     @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
   end
 end
