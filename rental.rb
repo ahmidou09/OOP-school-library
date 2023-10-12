056aa0606f3da8f6a69559b5757d8b20eb56c756
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-12 19:54:53 +0100

    Implemented Rental Class for Book-Person Association

---
+++ rental.rb
@@ -0,0 +1,16 @@
+require './book'
+require './person'
+
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(book, date, person)
+    @date = date
+    @book = book
+    @person = person
+
+    person.rentals << self
+    book.rentals << self
+  end
+end
